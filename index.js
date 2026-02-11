c88118f1e004878179ec9680c0b6dc4128b05577
xiaowanzi520 <88525927+xiaowanzi520@users.noreply.github.com>
2026-02-11 21:18:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 var rule = {
-  url: "https://ghp.xptvhelper.link/https://raw.githubusercontent.com/xiaobaitulele/xptv/refs/heads/main/main/all.json"
+  sites: [
+    {
+      name: "两个BT_兔",
+      type: 3,
+      api: "csp_bttwo_tu",
+      ext: "https://ghp.xptvhelper.link/https://raw.githubusercontent.com/xiaobaitulele/xptv/refs/heads/main/vod/bttwo_tu.js"
+    },
+    {
+      name: "4kvm_兔",
+      type: 3,
+      api: "csp_4kvm_tu",
+      ext: "https://ghp.xptvhelper.link/https://raw.githubusercontent.com/xiaobaitulele/xptv/refs/heads/main/vod/4kvm_tu.js"
+    },
+    {
+      name: "纽约影院_兔（无搜索）",
+      type: 3,
+      api: "csp_nycvod_tu",
+      ext: "https://ghp.xptvhelper.link/https://raw.githubusercontent.com/xiaobaitulele/xptv/refs/heads/main/vod/nycvod_tu.js"
+    }
+  ]
 };
 
 export default rule;
